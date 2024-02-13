d9c408a2a56d2d668841cd216185b79283f29477
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-13 07:06:47 +0100

    Update seed

--- db/migrate/20240120001856_add_profile_image_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddProfileImageToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :profile_image, :string, default: 'icons/user_icon.png'
-    User.update_all(profile_image: 'icons/user_icon.png')
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-# db/seeds.rb
-
 require 'open-uri'
 
 # Helper methods to create records
@@ -19,24 +17,16 @@ def create_user(username, email, password)
   user
 end
 
-def create_animal(name, animal_photo_url, date_of_birth, weight, escape_attempts, owner, genre)
-  puts "Creating animal with owner: #{owner.inspect}"
-  raise "Invalid owner parameter: #{owner}" unless owner.is_a?(User) && owner.valid?
-
-  animal = Animal.new(
+def create_animal(name, animal_photo, date_of_birth, weight, escape_attempts, owner, genre)
+  Animal.create(
     name: name,
+    animal_photo: animal_photo,
     date_of_birth: date_of_birth,
     weight: weight,
     escape_attempts: escape_attempts,
     owner_id: owner.id,
     genre: genre
   )
-
-  # Create Active Storage attachment for animal_photo
-  animal.animal_photo.attach(io: URI.open(animal_photo_url), filename: "#{name}_photo.jpg")
-
-  # Save the animal record
-  animal.save!
 end
 
 def create_specialization(name)
@@ -56,10 +46,10 @@ def create_vet(name, vet_photo, available_from, available_to, fees, bio, special
 end
 
 # Creating genres
-genre1 = create_genre('Cats')
-genre2 = create_genre('Fishes')
-genre3 = create_genre('Dinosaurs')
-genre4 = create_genre('Birds')
+genre1 = create_genre('Cat')
+genre2 = create_genre('Fishe')
+genre3 = create_genre('Dog')
+genre4 = create_genre('Bird')
 
 # Creating users
 user1 = create_user('AliceDoe', 'alice@example.com', 'password')
@@ -74,7 +64,15 @@ vet1 = create_vet('Dr. Taylor', 'https://picsum.photos/200', Time.parse('09:00')
 vet2 = create_vet('Dr. Anderson', 'https://picsum.photos/200', Time.parse('10:00'), Time.parse('18:00'), 75.0, 'Passionate about animals', specialization2)
 
 # Creating animals
-animal1 = create_animal('Tiger', 'https://picsum.photos/200', Date.new(2019, 7, 15), 180.2, 1, user1, genre1)
-animal2 = create_animal('Parrot', 'https://picsum.photos/200', Date.new(2022, 3, 8), 0.5, 0, user2, genre4)
+animal1 = create_animal('Sam', 'https://picsum.photos/200', Date.new(2019, 7, 15), 180.2, 1, user1, genre1)
+animal2 = create_animal('Olly', 'https://picsum.photos/200', Date.new(2022, 3, 8), 0.5, 0, user2, genre4)
+animal3 = create_animal('Tig', 'https://picsum.photos/200', Date.new(20121, 3, 15), 178.2, 1, user1, genre1)
+animal4 = create_animal('Parrot', 'https://picsum.photos/200', Date.new(2023, 3, 8), 0.5, 0, user2, genre4)
+animal5 = create_animal('Nomad', 'https://picsum.photos/200', Date.new(2014, 7, 15), 70.2, 1, user1, genre1)
+animal6 = create_animal('Onas', 'https://picsum.photos/200', Date.new(2010, 3, 8), 0.5, 0, user2, genre4)
+animal7 = create_animal('Eric', 'https://picsum.photos/200', Date.new(2019, 7, 15), 19.2, 1, user1, genre1)
+animal8 = create_animal('Max', 'https://picsum.photos/200', Date.new(2015, 3, 8), 0.5, 0, user2, genre4)
+animal9 = create_animal('Minox', 'https://picsum.photos/200', Date.new(2016, 7, 15), 67.2, 1, user1, genre1)
+animal10 = create_animal('Izabela', 'https://picsum.photos/200', Date.new(2017, 3, 8), 0.5, 0, user2, genre4)
 
 puts 'Seeding completed!'
