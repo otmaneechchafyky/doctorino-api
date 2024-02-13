b4967c4336fb9ff18da33891f689545b88dfa32a
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-13 07:04:40 +0100

    Update vet serializer to have only animal as output

--- app/serializers/animal_serializer.rb
+++ app/serializers/animal_serializer.rb
@@ -1,10 +1,10 @@
 class AnimalSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :animal_photo, :date_of_birth, :weight, :escape_attempts
+  attributes :id, :name, :animal_photo, :date_of_birth, :weight, :escape_attempts, :owner_id, :genre_id
   attribute :created_at do |animal|
     animal.created_at && animal.created_at.strftime('%m/%d/%Y')
   end
   attribute :updated_at do |animal|
-    animal.updated_at && animal.created_at.strftime('%m/%d/%Y')
+    animal.updated_at && animal.updated_at.strftime('%m/%d/%Y')
   end
 end
