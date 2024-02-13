3117065253db39e5d63a530201aaeddad2b54158
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-13 07:02:02 +0100

    Refactor vets json output

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -1,23 +1,14 @@
 class VetsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_vet, only: %i[show destroy update]
+  
   def index
-    @vets = []
     @vets_list = Vet.all
-    @vets_list.each do |vet|
-      @vets << {
-        data: VetSerializer.new(vet).serializable_hash[:data][:attributes],
-        specialization: SpecializationSerializer.new(vet.specialization).serializable_hash[:data][:attributes]
-      }
-    end
-    render json: @vets
+    render json: @vets_list, each_serializer: VetSerializer
   end
 
   def show
-    render json: {
-      data: VetSerializer.new(@vet).serializable_hash[:data][:attributes],
-      specialization: SpecializationSerializer.new(@vet.specialization).serializable_hash[:data][:attributes]
-    }
+    render json: VetSerializer.new(@vet).serializable_hash[:data][:attributes]
   end
 
   def destroy
@@ -29,7 +20,7 @@ class VetsController < ApplicationController
     @vet = Vet.new(vet_params)
     if @vet.save
         render json: { message: 'Vet created successfully.',
-        vet: VetSerializer.new(@vet).serializable_hash[:data][:attributes] },
+        VetSerializer.new(@vet).serializable_hash[:data][:attributes] },
         status: :created
     else
         render json: { errors: @vet.errors.full_messages }, status: :unprocessable_entity
@@ -39,7 +30,7 @@ class VetsController < ApplicationController
   def update
     if @vet.update(vet_params)
         render json: { message: 'Vet updated successfully.',
-        updated_vet: VetSerializer.new(@vet).serializable_hash[:data][:attributes] },
+        VetSerializer.new(@vet).serializable_hash[:data][:attributes] },
         status: :ok
     else
         render json: { errors: @vet.errors.full_messages }, status: :unprocessable_entity
