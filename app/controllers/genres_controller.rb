1132ef411a3cdb3a4a339c441b970735340f6068
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-13 07:01:50 +0100

    Refactor the genres json output

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,11 +1,9 @@
 class GenresController < ApplicationController
   before_action :authenticate_user!
+  
   def index
     @genres = []
     @genres_list = Genre.all
-    @genres_list.each do |genre|
-      @genres << GenreSerializer.new(genre).serializable_hash[:data][:attributes]
-    end
-    render json: @genres
+    render json: @genres_list, each_serializer: GenreSerializer
   end
 end
