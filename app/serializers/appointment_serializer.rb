b9bbcbe39bceb11367822601f7e4e5c4607af30b
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-13 07:05:13 +0100

    Update vet serializer to have only appointment as output

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,6 +1,6 @@
 class AppointmentSerializer
   include JSONAPI::Serializer
-  attributes :id, :date, :time, :location, :duration
+  attributes :id, :date, :time, :location, :duration, :animal_id, :vet_id
   attribute :created_at do |appointment|
     appointment.created_at && appointment.created_at.strftime('%m/%d/%Y')
   end
