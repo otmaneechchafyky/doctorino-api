98fb738809f99f364a1173c27e0a802c26a5e011
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-20 00:23:08 +0100

    Update appointmentController

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -13,7 +13,7 @@ class AppointmentsController < ApplicationController
     def create
         @appointment = Appointment.new(appointment_params)
         if @appointment.save
-            render json: {message: 'Appointment created successfully.'}, status: ok
+            render json: { message: 'Appointment created successfully.' }, status: :ok
         else
             render json: { errors: @appointment.errors.full_messages }, status: :unprocessable_entity
         end
